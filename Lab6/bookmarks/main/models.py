a3477d550a411793f49646c19033b924d80a837d
telbohtimy <elbohtim@ualberta.ca>
2015-02-24 10:38:27 -0700

    lab6:

---
+++ Lab6/bookmarks/main/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Tag(models.Model):
+    name=models.CharField(max_length=128,unique=True)
+    def __unicode__(self):
+        return self.name
+class Link(models.Model):
+    title=models.CharField(max_length=128,unique=True)
+    url=models.URLField()
+    tags=models.ManyToManyField(Tag)
+    
+    def __unicode__(self):
+        return self.title
